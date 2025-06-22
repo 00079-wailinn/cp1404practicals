0847b35a30dee904d5a9859154e4c32d2ce6d46d
00079-wailinn <wailinnaung@my.jcu.edu.au>
2025-06-22 14:02:35 +0800

    prac5

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,37 +1,14 @@
+"""Add commentMore actions
+Text: this is a collection of words of nice words this is a fun thing it is
+a : 2
+collection : 1
+fun : 1
+is : 3
+it : 1
+nice : 1
+of : 2
+thing : 1
+this : 2
+words : 2
 """
-Word Occurrences
-Estimate: 20 minutes
-Actual:   32 minutes
-"""
 
-"""
-CP1404 Practical - Suggested Solution
-Email to name dictionary
-"""
-
-
-def main():
-    """Create dictionary of emails-to-names."""
-    email_to_name = {}
-    email = input("Email: ")
-    while email != "":
-        name = get_name_from_email(email)
-        confirmation = input(f"Is your name {name}? (Y/n) ")
-        if confirmation.upper() != "Y" and confirmation != "":
-            name = input("Name: ")
-        email_to_name[email] = name
-        email = input("Email: ")
-
-    for email, name in email_to_name.items():
-        print(f"{name} ({email})")
-
-
-def get_name_from_email(email):
-    """Extract expected name from email address."""
-    prefix = email.split('@')[0]
-    parts = prefix.split('.')
-    name = " ".join(parts).title()
-    return name
-
-
-main()
