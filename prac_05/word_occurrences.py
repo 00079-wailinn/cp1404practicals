d157ced2373776d10a0542f15d3ec95bd7b48e92
00079-wailinn <wailinnaung@my.jcu.edu.au>
2025-06-22 14:02:52 +0800

    prac5

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -12,3 +12,14 @@ this : 2
 words : 2
 """
 
+sentence = input("Enter a sentence: ")
+characters = sentence.split()
+print(characters)
+
+dict_characters = {}
+for character in characters:
+    dict_characters[character] = characters.count(character)
+
+max_length = max((len(word) for word in dict_characters))
+for character, count_character in sorted(dict_characters.items()):
+    print(f"{character:{max_length}} : {count_character}")
